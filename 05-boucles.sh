2d0a0fbee7ed6131441bf3463cc93b33c4cb7a18
Vincent Rochon <vincent.rochon8@gmail.com>
2018-12-03 10:51:22 -0500

    File 05 Changed

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
 
+# Boucle For
+for ((i=0;i<5;i++))
+do
+	echo $i
+done
+
+# Execute une commande, prend le resultat et le met dans la variable Liste
+liste=`ls`
+
+# Pour chacun des éléments de la liste... fais (for each...)
+for fic in $liste
+do
+	echo $fic
+done
+resultat=o
+
+# -o = or
+# -a = and 
+# boucle tant que (while...)
+while test $resultat = o -o $resultat = O
+do
+	read -p "Entez o pour répéter la boucle : " resultat
+done
+
+
+
