8356f51287464ba6b92eef69fdc85144cfa81c9d
Vincent Rochon <vincent.rochon8@gmail.com>
2018-12-03 10:56:27 -0500

    File 06 changed

--- 06-tableau.sh
+++ 06-tableau.sh
@@ -3,12 +3,12 @@
 # Declare le tableau
 declare -a nom=(Back to the Futur)
 
-# Element 1
+# Element 1 (Back)
 echo ${nom[0]}
 
-# Element 2
+# Element 2 (to)
 echo ${nom[1]}
 
-# Nombre element
+# Nombre element (4)
 echo ${#nom[@]}
 
