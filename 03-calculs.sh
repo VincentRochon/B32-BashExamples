9be68cc99b8c6f9ef14749008b3a3fe61b21f9a9
Vincent Rochon <vincent.rochon8@gmail.com>
2018-12-03 10:09:19 -0500

    file 03 done

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+# Sans $((formule )), echo donneras la formule et pas le resultat
+echo 10+19
+echo $((10+19))
+
+# Facon plus "clean" de faire des calcules
+# Demande deux variable(premier & second) a l'utilisateur
+read -p "nombre 1 :" premier
+read -p "nombre 2 :" second
+
+# Calcule valeur de premier * second (voir + sur let)
+let resultat=$premier*$second
+
+echo $resultat
