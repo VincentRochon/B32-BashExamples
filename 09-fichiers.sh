2b18ca102ad85a89c4a5d8f81e0da0508c8eacb1
Vincent Rochon <vincent.rochon8@gmail.com>
2018-12-03 11:16:39 -0500

    File 09 changed

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+# Test si le fichier demander a l'argument 1 existe ( -e = voir s'il est existant)
+if test -e $1
+then
+ 	echo "existe"
+
+# If dans un If, demande si fichir ou pas ( -d = repertoire)
+	if test -d $1
+	then
+		echo "Il est un répertoire"
+	fi
+else 
+	echo "inexistant"
+fi
