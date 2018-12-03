1afd646d08728b00975835d13058486e410d6cfe
Vincent Rochon <vincent.rochon8@gmail.com>
2018-12-03 12:07:31 -0500

    Exercice 02 Changed

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+Menu(){
+	echo "===================="
+	echo "Cvm-Soft"
+	echo "Q pour quitter"
+	echo "D pour dctionnaire"
+	echo "===================="
+}
+choix='a' # forcer l'entrer dans la boucle et initialisation de choix 
+while test $choix != 'q' -a $choix != 'Q'
+do
+	Menu
+	read -p "Quel est votre choix : " choix
+	if test $choix = 'd'
+	then
+		./exercice1.sh
+	fi
+	if test $choix = 'D'
+	then
+		./exercice1.sh
+	fi
+done
