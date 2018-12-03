fa671fd6ac2dc9c5fd7ed27850f16728bf2839c8
Vincent Rochon <vincent.rochon8@gmail.com>
2018-12-03 11:10:24 -0500

    file 08 changed

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+echo -n "Texte : "
+read  texte
+
+# ~ = ce qui suit va etre une expression reguliere
+# [[   ]] remplace test
+# Commence par  (^)
+# Termine par  ($)
+# Au moins une fois, mais peut aller a l'infini (+)
+if [[ $texte =~ ^[0-9]+$ ]]
+then
+	echo "Numérique"
+fi
