fc5828cd4ca2b94b4820bf82f052b969e8fd70ec
Vincent Rochon <vincent.rochon8@gmail.com>
2018-12-03 12:32:43 -0500

    Exercice 03 changed

--- exercice3.sh
+++ exercice3.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+compteur=0
+for ((i=0;i<15;i++))
+do
+	espace=i
+	for ((j=0;j<$espace;j++))
+	do
+		echo -n " "
+	done
+	echo -n "z"
+	sleep 0.2
+	clear
+	compteur=$compteur+1
+done
+
+	for((i=0;i<$compteur;i++))
+	do
+		echo -n " "
+	done
+	echo -n  " BOOM"
+	sleep 5
+	clear
