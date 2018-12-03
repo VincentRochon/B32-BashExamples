c65bba271117456e9f4a128aabb313e5f205cc81
Vincent Rochon <vincent.rochon8@gmail.com>
2018-12-03 10:02:11 -0500

    02 file changed

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
 
+# Aucun espace, si espace alors erreur; pas d'espace entre opérateur et variables
+prenom=Vince
+echo $prenom
+
+# Convention des variables
+nomDeFamille="a l l o"
+
+# Force une déclaration d'une variable "int"
+declare -i age=33
+
+# Mettre une variable READ ONLY 
+declare -r age2=33
+
+# Les variables system (Pas nous qui défini)
+
+# $0 = nom de l' executable (1er partie de l'appel)
+echo $0
+
+# $1 = Premiere Argument lors de l'appel
+echo $1
+
+# $2 = Deuxieme Argument lors de l'appel (va jusqu'a 9)
+echo $2 
+
+# $# =  le nombre d'argument appeler
+echo $#
+
