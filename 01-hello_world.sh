d414b68e1672befb31fd8814ece1c70af8b21e4f
Vincent Rochon <vincent.rochon8@gmail.com>
2018-12-03 09:49:31 -0500

    File 01 Done

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+# Ceci est un commentaire
+echo "Salut"
+
+# -n = empeche le retour de ligne 
+echo -n "Vive "
+echo "Linux"
+
+# -e = Permet l'utilisation des caractères speciaux (\n saute une ligne)
+echo  "123\n123\n123"
+
+# read = cin (Permet entrez de valeur) resultat de read = variable age, 
+# Assignation de variable = age (pas de $)
+# Utilisation de valeur = $age ($ pour obtenir la valeur de la variable)
+read -p "Vous avez quel age?" age
+echo $age "ans! Vous paraissez plus jeune"
+
+
